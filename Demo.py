182a40f07994f12107c29db5b9e656cea1505c0a
jaweria01 <120401966+jaweria01@users.noreply.github.com>
2024-06-09 01:04:22 -0700

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -17,8 +17,8 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding="ISO-8859-1")
 else:
-    os.chdir(r'C:\Users\ADMIN\Documents\SalesProject')
-    df = pd.read_csv('traincsvFormated(Edited3).csv', encoding="ISO-8859-1")
+    os.chdir(r"C:\Users\ADMIN\Documents\SalesProject")
+    df = pd.read_csv("traincsvFormated(Edited3).csv", encoding="ISO-8859-1")
 # Ensure all columns have compatible types for Arrow serialization
 for column in df.columns:
     if df[column].dtype == 'object':
